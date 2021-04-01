c50f7921d97711581bd08e65a82fa318c6cdc048
repeiras <69979166+repeiras@users.noreply.github.com>
2021-04-01 15:50:12 -0300

    Update Ex01.c

--- Aula1/Ex01.c
+++ Aula1/Ex01.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 int main(void) {
-  printf("Ola Mundo");
-  printf("Meu Nome é Rafael");
+  printf("Ola Mundo \n");
+  // \n serve para pular linha, enquanto // para colocar comentario
+  printf("Meu Nome é Rafael \n");
   return 0;
 }
