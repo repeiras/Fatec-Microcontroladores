dde17a1b16bd4bef340393f124e2c66f1fa990ec
repeiras <69979166+repeiras@users.noreply.github.com>
2021-06-06 21:54:07 -0300

    Delete Aula1 directory

--- Aula1/Ex01.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(void) {
-  printf("Ola Mundo \n");
-  // \n serve para pular linha, enquanto // para colocar comentario
-  printf("Meu Nome é Rafael \n");
-  return 0;
-}
